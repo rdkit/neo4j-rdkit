848cb896eff6873eafbd5233aabe32486178a9ff
Evgeny Sorokin <evgerher1l@gmail.com>
2019-06-19 01:48:03 +0300

    Enable building valid file for neo4j extension manager
    1) Unable to copy .jar file using docker-compose
    2) Have to start server, docker cp .jar into .../plugins
    3) Restart server with plugin

--- src/main/java/org/rdkit/neo4j/analyzer/RDKitAnalyzer.java
+++ src/main/java/org/rdkit/neo4j/analyzer/RDKitAnalyzer.java
@@ -1,5 +1,6 @@
 package org.rdkit.neo4j.analyzer;
 
+import java.io.IOException;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
@@ -8,10 +9,24 @@ public class RDKitAnalyzer extends Analyzer {
 
   @Override
   protected TokenStreamComponents createComponents(String s) {
-//    Tokenizer source = new FooTokenizer(reader);
-//    TokenStream filter = new FooFilter(source);
-//    filter = new BarFilter(filter);
-//    return new TokenStreamComponents(source, filter);
-    return null;
+    Tokenizer source = new Tokenizer() {
+      @Override
+      public boolean incrementToken() throws IOException {
+        return false;
+      }
+    };
+    TokenStream filter = new TokenStream(source) {
+      @Override
+      public boolean incrementToken() throws IOException {
+        return false;
+      }
+    };
+    filter = new TokenStream(filter) {
+      @Override
+      public boolean incrementToken() throws IOException {
+        return false;
+      }
+    };
+    return new TokenStreamComponents(source, filter);
   }
 }
