1bb2b8a181edede1d299291d6215f7196fc624b3
Evgeny Sorokin <evgerher1l@gmail.com>
2019-08-25 14:19:39 +0300

    Finish license plugin

--- src/test/java/org/rdkit/neo4j/index/BitSetIndexQueryingTest.java
+++ src/test/java/org/rdkit/neo4j/index/BitSetIndexQueryingTest.java
@@ -1,5 +1,20 @@
 package org.rdkit.neo4j.index;
 
+/*-
+ * #%L
+ * RDKit-Neo4j
+ * %%
+ * Copyright (C) 2019 RDKit
+ * %%
+ * Copyright (C) 2019 Evgeny Sorokin
+ * @@ All Rights Reserved @@
+ * This file is part of the RDKit Neo4J integration.
+ * The contents are covered by the terms of the BSD license
+ * which is included in the file LICENSE, found at the root
+ * of the neo4j-rdkit source tree.
+ * #L%
+ */
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
